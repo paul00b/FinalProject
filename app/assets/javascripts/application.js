8aa2448d894c86e5e54ae80f635657280433de44
paul00b <broussolle.paul@gmail.com>
2019-02-22 17:29:30 +0100

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,68 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require js/jquery
+//= require js/popper
+//= require js/tooltip
+
+//= require bootstrap/js/bootstrap.bundle
+//= require bootstrap/js/bootstrap
+//= require js/jquery.nicescroll
+//= require js/moment
+//= require js/custom
+//= require js/scripts
+//= require js/stisla
+//= require js/sparkline
+//= require chocolat/js/jquery.chocolat
+//= require js/Chart
+//= require OwlCarousel/owl.carousel
+//= require jqvmap/jquery.vmap
+//= require js/pwstrength
+//= require js/page/index
+
+
+// = require js/page/auth-register
+// = require js/page/bootstrap-modal
+//= require js/page/components-chat-box
+//= require js/page/components-multiple-upload
+//= require js/page/components-statistic
+//= require js/page/components-table
+//= require js/page/components-user
+//= require js/page/features-post-create
+//= require js/page/features-posts
+//= require js/page/features-setting-detail
+//= require js/page/forms-advanced-forms
+//= require js/page/gmaps-advanced-route
+//= require js/page/gmaps-draggable-marker
+//= require js/page/gmaps-geocoding
+//= require js/page/gmaps-geolocation
+//= require js/page/gmaps-marker
+//= require js/page/gmaps-multiple-marker
+//= require js/page/gmaps-route
+//= require js/page/gmaps-simple
+//= require js/page/index-0
+//= require js/page/modules-calendar
+//= require js/page/modules-chartjs
+//= require js/page/modules-datatables
+//= require js/page/modules-ion-icons
+//= require js/page/modules-slider
+//= require js/page/modules-sparkline
+//= require js/page/modules-sweetalert
+//= require js/page/modules-toastr
+//= require js/page/modules-vector-map
+//= require js/page/utilities-contact
+
+
